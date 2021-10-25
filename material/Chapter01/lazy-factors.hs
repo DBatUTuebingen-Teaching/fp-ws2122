59e45007831f909ff10831da2a7fb00c02102e22
Torsten Grust <torsten.grust@gmail.com>
2021-10-25 16:22:09 +0200

    Minor fix in lazy-factors example code

--- material/Chapter01/lazy-factors.hs
+++ material/Chapter01/lazy-factors.hs
@@ -4,7 +4,7 @@ factors n = [ m | m <- [2..n-1], mod n m == 0 ]
 
 -- To see the effects of lazy evaluation in ghci:
 --
--- 1.  fs = factor 55555
+-- 1.  fs = factors 55555
 -- 2.  :sprint fs
--- 3.  head fs
+-- 3.  fs == []
 -- 4.  :sprint fs
