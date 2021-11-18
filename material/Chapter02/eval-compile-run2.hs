f212fef4a3da8eaf5376baa39427b3f5df98fed2
Torsten Grust <torsten.grust@gmail.com>
2021-11-18 13:53:41 +0100

    Example code for expression evaluation/compilation/execution now complete

--- material/Chapter02/eval-compile-run2.hs
+++ material/Chapter02/eval-compile-run2.hs
@@ -19,10 +19,51 @@ ex1 = Add (Mul (Lit 5) (Lit 8)) (Lit 2)  --  (5 * 8) + 2
 ex2 :: Exp Float
 ex2 = Sub (Lit 42.1) (Lit 0.1)           -- 42.1 - 0.1
 
+-- The instruction set of a super-simple stack machine
+data Instr a = Push a  -- place literal a on stack top
+             | DoAdd   -- add the two top-most stack entries, result at stack top
+             | DoSub
+             | DoMul
+  deriving (Show)
+
+-- A program is a sequence of instructions
+type Program a = [Instr a]
+-- A stack is a sequence of values (stack top = head)
+type Stack a = [a]
+
+
+-- Compile an expression into a program
+compile :: Exp a -> Program a
+compile (Lit n)     = [Push n]
+compile (Add e1 e2) = compile e1 ++ compile e2 ++ [DoAdd]
+compile (Sub e1 e2) = compile e1 ++ compile e2 ++ [DoSub]
+compile (Mul e1 e2) = compile e1 ++ compile e2 ++ [DoMul]
+
+
+-- Run a program against an initial stack
+-- (leaves the program result on the stack top)
+run :: Num a => Stack a -> Program a -> Stack a
+run st []             = st
+run st       ((Push n):ins) = run (n:st)       ins
+run (x:y:st) (DoAdd:ins)    = run ((y + x):st) ins
+run (x:y:st) (DoSub:ins)    = run ((y - x):st) ins
+run (x:y:st) (DoMul:ins)    = run ((y * x):st) ins
+run _        _              = error "VM program broken"
+
 main :: IO ()
 main = do
   print $ ex1
   print $ ex2
   print $ eval ex1
   print $ eval ex2
+  print $ compile ex1
+  print $ compile ex2
+  print $ (run [] . compile) ex1
+  print $ (run [] . compile) ex2
+
+
+
+
+
+
 
